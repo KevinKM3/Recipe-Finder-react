38b7634a0a210ee0bb2b5601bd6b027492cf2ed5
Kevin Matthews <kevmatthews3@gmail.com>
2020-05-01 23:18:36 -0400

    search-bar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ const App = () => {
   const APP_ID = "02593889";
   const APP_KEY = "f1a27600b3af976b81545aa415a13ff1	";
 
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
-    <div>
-      <h1>Kev's React Recipes</h1>
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
